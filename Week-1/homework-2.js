911e6aced47d7567cfb4ebc5a5501a8148aa65a8
Alper SEVİNÇLER <alper.sevincler@gmail.com>
2025-03-05 11:52:20 +0300

    Update homework-2.js

--- Week-1/homework-2.js
+++ Week-1/homework-2.js
@@ -30,6 +30,29 @@ console.log(totalOrder300); //çıktı: Ahmet
 
 // ---Zor Soru: Şehir bazında toplam sipariş miktarlarını döndür.
 
-const cityOrdersTotals = customers.reduce((acc, user) => {acc[user.city] = (acc[user.city] || 0) + ( user.orders.reduce((total, order)  => total + order, 0) ); return acc}, {});
+const cityOrdersTotals = customers.reduce((acc, user) => {
+  acc[user.city] = (acc[user.city] || 0) + ( user.orders.reduce((total, order)  => total + order, 0) ); 
+  return acc
+}, {});
 
 console.log(cityOrdersTotals); //çıktı: { Ankara: 750, 'İstanbul': 350, 'İzmir': 800, Bursa: 0 }
+
+
+// ---Çok Zor Soru: Her müşterinin toplam sipariş miktarını ve yaşını bir string olarak şu formatta döndür: Ahmet (32): 450 TL
+
+const userAgeTotalOrders = customers.map((user) => {
+  const userTotalOrders = user.orders.reduce((acc, order) => acc + order, 0);
+  return `${user.name} (${user.age}): ${userTotalOrders} TL`;
+});
+
+console.log("userAgeTotalOrders = ", userAgeTotalOrders);
+/*
+çıktı:
+userAgeTotalOrders =  [
+  'Ahmet (32): 450 TL',
+  'Ayşe (27): 350 TL',
+  'Mehmet (40): 800 TL',
+  'Fatma (35): 300 TL',
+  'Zeynep (28): 0 TL'
+]
+*/
